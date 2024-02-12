89b4d863207cba62b2a56800e1e9796ab0a92beb
eran-barkai <35926069+eran-barkai@users.noreply.github.com>
2024-02-12 14:15:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {JSONPath} = require('jsonpath-plus');
 
 const replacementContent = 'Will be replaced with HMAC of request body';
 
-function hmac(content, options) {
+function hmac(content, options, queryString = null) {
   if (options.jsonPath) {
     content = JSON.stringify(JSONPath({
       path: options.jsonPath,
@@ -14,15 +14,18 @@ function hmac(content, options) {
   if (options.removeWhitespace) {
     content = JSON.stringify(JSON.parse(content));
   }
+  if (options.queryParam) {
+    content = queryString;
+  }
   const hash = crypto.createHmac(options.algorithm, options.key);
   hash.update(content, 'utf8');
   return hash.digest(options.encoding);
 }
 
-function replaceWithHMAC(content, body) {
+function replaceWithHMAC(content, body, queryString) {
   return content.replace(new RegExp(replacementContent + ' \\(([a-f0-9]+)\\)', 'g'), (match, hex) => {
     const options = JSON.parse(Buffer.from(hex, 'hex').toString('utf-8'));
-    return hmac(body, options)
+    return hmac(body, options, queryString)
   });
 }
 
@@ -71,12 +74,16 @@ module.exports.templateTags = [{
       placeholder: 'HMAC Secret Key'
     },
     {
+      displayName: 'use Query Params as value',
+      type: 'boolean',
+    },
+    {
       displayName: 'Message',
       type: 'string',
       placeholder: 'Message to hash (leave empty to use request body)'
     }
   ],
-  async run(context, algorithm, encoding, removeWhitespace = false, jsonPath = '', key = '', value = '') {
+  async run(context, algorithm, encoding, removeWhitespace = false, jsonPath = '', key = '', queryParam, value = '') {
     if (encoding !== 'hex' && encoding !== 'base64') {
       throw new Error(`Invalid encoding ${encoding}. Choices are hex, base64`);
     }
@@ -90,9 +97,11 @@ module.exports.templateTags = [{
       key: key,
       algorithm: algorithm,
       encoding: encoding,
+      queryParam: queryParam === true || queryParam === 'true' ? true : undefined,
       jsonPath: jsonPath !== '' ? jsonPath : undefined,
       removeWhitespace: removeWhitespace === true || removeWhitespace === 'true' ? true : undefined
     };
+
 
     if (value === '') {
       return replacementContent + ' (' + Buffer.from(JSON.stringify(options)).toString('hex') + ')';
@@ -105,26 +114,32 @@ module.exports.templateTags = [{
 module.exports.requestHooks = [
   async context => {
     const body = context.request.getBody();
+    const queryParams = context.request.getParameters();
+    const queryString = queryParams.map((x) => Object.values(x).join('=')).join('&')
+
     let bodyText = body.text || '';
+
+
     if (bodyText.indexOf(replacementContent) !== -1) {
-      bodyText = replaceWithHMAC(bodyText, bodyText);
+      bodyText = replaceWithHMAC(bodyText, bodyText, queryString);
       context.request.setBody({
         ...body,
         text: bodyText,
       });
     }
     if (context.request.getUrl().indexOf(replacementContent) !== -1) {
-      context.request.setUrl(replaceWithHMAC(context.request.getUrl(), bodyText));
+      context.request.setUrl(replaceWithHMAC(context.request.getUrl(), bodyText, queryString));
     }
     context.request.getHeaders().forEach(h => {
       if (h.value.indexOf(replacementContent) !== -1) {
-        context.request.setHeader(h.name, replaceWithHMAC(h.value, bodyText));
+        context.request.setHeader(h.name, replaceWithHMAC(h.value, bodyText, queryString));
       }
     });
     context.request.getParameters().forEach(p => {
       if (p.value.indexOf(replacementContent) !== -1) {
-        context.request.setParameter(p.name, replaceWithHMAC(p.value, bodyText));
+        context.request.setParameter(p.name, replaceWithHMAC(p.value, bodyText, queryString));
       }
     });
   }
+
 ];
